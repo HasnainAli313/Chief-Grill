4e128a15ac41c4ab49f0e89e4325663e5275378c
Hasnain Ali <93475112+HasnainAli313@users.noreply.github.com>
2024-09-12 21:54:22 +0500

    added OrientalTaste componenet

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import BestSeller from '../components/BestSeller'
 import Hero from '../components/Hero'
+import OrientalTaste from '../components/OrientalTaste'
 function Home() {
   return (
     <>
       <Hero></Hero>
       <BestSeller></BestSeller>
+      <OrientalTaste></OrientalTaste>
     </>
   )
 }
