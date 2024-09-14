d30f7a85012b431de469872cde8c83b278e7d840
Hasnain Ali <93475112+HasnainAli313@users.noreply.github.com>
2024-09-14 17:55:17 +0500

    added little animation

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -7,22 +7,22 @@ function Statistics() {
         
         <div data-aos="fade-up"
     data-aos-duration="3000" className="flex justify-around flex-wrap gap-5 mt-10">
-            <div className="h-[120px] flex flex-col place-content-center w-[120px] rounded-[50%] p-1  border ">
+            <div className="h-[120px] hover:scale-[1.1] transition-all duration-500 flex flex-col place-content-center w-[120px] rounded-[50%] p-1  border ">
                 <h4 className=" font-semibold text-xl text-[#ffc404]">123</h4>
                 <h4 className=" font-semibold text-xl text-white">OUTLETS</h4>
             </div>
            
-            <div className="h-[120px] flex flex-col place-content-center w-[120px] rounded-[50%] p-1  border ">
+            <div className="h-[120px] hover:scale-[1.1] transition-all duration-500 flex flex-col place-content-center w-[120px] rounded-[50%] p-1  border ">
                 <h4 className=" font-semibold text-xl text-[#ffc404]">100</h4>
                 <h4 className=" font-semibold text-xl text-white">CHEF</h4>
             </div>
            
-            <div className="h-[120px] flex flex-col place-content-center w-[120px] rounded-[50%] p-1  border ">
+            <div className="h-[120px] hover:scale-[1.1] transition-all duration-500 flex flex-col place-content-center w-[120px] rounded-[50%] p-1  border ">
                 <h4 className=" font-semibold text-xl text-[#ffc404]">300</h4>
                 <h4 className=" font-semibold text-xl text-white">MENU</h4>
             </div>
            
-            <div className="h-[120px] flex flex-col place-content-center w-[120px] rounded-[50%] p-1  border ">
+            <div className="h-[120px] hover:scale-[1.1] transition-all duration-500 flex flex-col place-content-center w-[120px] rounded-[50%] p-1  border ">
                 <h4 className=" font-semibold text-xl text-[#ffc404]">30</h4>
                 <h4 className=" font-semibold text-xl text-white">COUNTRY</h4>
             </div>
